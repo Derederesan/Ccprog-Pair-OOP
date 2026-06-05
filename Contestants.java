e7aa3dd342082491be7a7e48318b66aee3b8de66
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-06-05 08:56:43 +0800

    Add files via upload

---
+++ Contestants.java
@@ -0,0 +1,51 @@
+/*
+
+     @author Alexie Ilagan 
+     @author Aki Nicole Cartago 
+
+*/
+public class Contestant
+{
+   
+
+    public Contestant(String name, String country,int age)
+    {
+        
+    }
+    public Contestant(String name, String country)
+    {
+
+    }
+    public String getName()
+    {
+
+    }
+    public String getCountry()
+    {
+
+    }
+    public void addTalent(String [] t)//finish 
+    {
+
+    }
+    public boolean canCompete(String [] t)
+    {
+
+    }
+    public boolean canCompete(String [] t, int maxAge)
+    {
+
+    }
+    public boolean canCompete(int minAge)
+    {
+
+    }
+    public String toString()
+    {
+
+    }
+    private String name; 
+    private String country; 
+    private String[] talents; 
+    private int age;  
+}
---
+++ Pageants.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Pageant
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(); 
+        int y, tempAge; 
+        String tempName, tempCountry; 
+        //declare 5 contestant objects
+        Contestant[] queens = new Contestant[5]; 
+        //loop asking input of name country and age of beauty queen contestants 
+        for(y=0; y<queens.length;y++)
+        {
+            System.out.println("Input contestant name: "); 
+            tempName= sc.nextLine();
+            System.out.println("Input contestant country: "); 
+            System.out.println("Input contestant name: "); 
+
+        }
+        //ask user to input 2 talents-used for 1st contestant 
+        //check if user can compete in singing 
+        //display result of checking 
+            //true only if first contestant is at least 25 years old 
+    }
+}
