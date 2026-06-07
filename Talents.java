43d1464da5e4b7bbce9ca254bf495195c02092cc
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-06-08 01:06:18 +0800

    Update Talents.java

--- Talents.java
+++ Talents.java
@@ -1,24 +1,32 @@
 /*
-    @author Aki Nicole
+@author Aki Nicole
 */
 
 import java.util.*;
 
 public class Talents
 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
 
-        String talent1, talent2;
+```
+    //stores talents entered by the user
 
-        System.out.print("Input talent 1: ");
-        talent1 = sc.nextLine();
+    String talent1, talent2;
 
-        System.out.print("Input talent 2: ");
-        talent2 = sc.nextLine();
+    //asks user to input 2 talents
 
-        queens[0].addTalent(talent1);
-        queens[0].addTalent(talent2);
-    }
+    System.out.print("Input talent 1: ");
+    talent1 = sc.nextLine();
+
+    System.out.print("Input talent 2: ");
+    talent2 = sc.nextLine();
+
+    //adds the talents to the first contestant object
+
+    queens[0].addTalent(talent1);
+    queens[0].addTalent(talent2);
+}
+
 }
