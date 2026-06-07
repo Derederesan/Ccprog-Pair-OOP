e36a34946a3bd8dab8e74d3526da25846efdbf63
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-06-07 21:06:23 +0800

    Update Contestants.java

--- Contestants.java
+++ Contestants.java
@@ -6,43 +6,70 @@
 */
 public class Contestant
 {
-   
-
-    public Contestant(String name, String country,int age)
+    public Contestant(String n, String c,int a)
     {
-        
+          //initializes name, country and age 
+         name= n; 
+         country = c; 
+         a = age; 
     }
-    public Contestant(String name, String country)
+    public Contestant(String n, String c)
     {
-
+          //initializes name and country 
+         name= n; 
+         country = c; 
     }
     public String getName()
     {
-
+          //returns name of participant 
+         return name; 
     }
     public String getCountry()
     {
-
+          //returns country participant is representing 
+         return country; 
     }
-    public void addTalent(String [] t)//finish 
+    public void addTalent(String t)//finish 
     {
-
+         //if array is not fully filled(at least 1 string not initialized)
+          //add parameter to list 
+         
     }
-    public boolean canCompete(String [] t)
+    public boolean canCompete(String t)
     {
-
+          //checks if parameter matches defined talents of participant 
+         int y; 
+         boolean value; 
+         for(y=0; y<talents.length; y++)
+          {
+               if(t[y]==talents[y])
+               {
+                    value=true; 
+               }
+          }
+         return value; 
     }
-    public boolean canCompete(String [] t, int maxAge)
+    public boolean canCompete(String t, int maxAge)
     {
-
+          //true if matches defined talents of participant 
+              //also if participant is at most maxAge 
+         
     }
     public boolean canCompete(int minAge)
     {
-
+          //returns true if participant is at least equal to minAge 
+         boolean value=false;
+         if(age >= minAge)
+          {
+              value=true; 
+          }
+         
+         return value; 
     }
     public String toString()
     {
-
+          //returns concatenation of name, country and age  
+         return name + " " + country + " " + age; 
     }
     private String name; 
     private String country; 
