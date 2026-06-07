de21979127a63c007250b21cd1cc6eb2c0e9fab2
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-06-08 01:00:08 +0800

    Create Talents.java

---
+++ Talents.java
@@ -0,0 +1,24 @@
+/*
+    @author Aki Nicole
+*/
+
+import java.util.*;
+
+public class Talents
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String talent1, talent2;
+
+        System.out.print("Input talent 1: ");
+        talent1 = sc.nextLine();
+
+        System.out.print("Input talent 2: ");
+        talent2 = sc.nextLine();
+
+        queens[0].addTalent(talent1);
+        queens[0].addTalent(talent2);
+    }
+}
