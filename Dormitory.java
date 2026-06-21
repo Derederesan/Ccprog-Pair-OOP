4e34a2ab8f9b958fe792bfefea62737f9c4c4e86
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-06-21 21:18:38 +0800

    Add files via upload

---
+++ Dormitory.java
@@ -0,0 +1,13 @@
+public class Dormitory
+{
+    public dormitory()
+    {
+
+    }
+
+    
+    private String name; //dormitory name
+    private int numRoom; //number of rooms provided 
+    
+
+}
---
+++ Room - template.java
@@ -0,0 +1,72 @@
+public class Room
+{
+	public Room(int roomNum, int maxCapacity)
+	{
+              //Provide code 
+			  //provide number
+			this.NUM=roomNum; 
+			  //provide maximum capacity 
+			this.MAX=maxCapacity;
+	}
+	
+	public boolean addGuest(Person guest)
+	{ 
+              //Provide code 
+			int y; 
+			  //check if capacity is full 
+			if()
+			{
+				for(y=0;y<6;y++)
+				{
+					
+				}
+			}
+			else
+			{
+				for(y=0; y<6;y++)
+			}
+			  //check for null
+			  //add guest to room 
+	}
+	
+	public boolean isFull()
+	{
+              //Provide code 
+	}
+	
+	public boolean isEmpty()
+	{
+		return size == 0;
+	}
+	
+	public int getMaxCapacity()
+	{
+		return MAX;
+	}
+
+	public int getRoomNum()
+	{
+		return NUM;
+	}
+	
+	public Person[] getGuests()
+	{
+		return guests;
+	}
+	
+	public int getNumOccupants()
+	{
+		return size;
+	}
+	
+	public void removeGuest(int index)
+	{
+              //Provide code 
+			  index--;
+	}
+	
+	private final int MAX; //max number of guests
+	private final int NUM; //room number
+	private Person[] guests;  //do not change to ArrayList
+	private int size; //current number of guests in room
+}
