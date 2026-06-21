3a2d32454c1bda2a69cb653f055f6b7e0e2899aa
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-06-21 21:48:22 +0800

    Update Dormitory.java

--- Dormitory.java
+++ Dormitory.java
@@ -1,11 +1,19 @@
 public class Dormitory
 {
-    public dormitory()
+    private dormitory()
     {
-
+        
     }
 
-    
+    public boolean checkAddGuest(String name, String nationality, int roomNum)
+    {
+        //if capacity is full or invalid room number provided
+        if(
+        //else true 
+    }
+
+    public 
+    //note:look for how to check invalid room number 
     private String name; //dormitory name
     private int numRoom; //number of rooms provided 
     
